87d08f779fb21f9eac3979292cde9225a8bd5a47
Bryan Murdock <bmurdock@gmail.com>
2023-04-29 08:20:27 -0600

    some basic structure for the scanner

--- pylox/lox.py
+++ pylox/lox.py
@@ -9,20 +9,48 @@ def parse_args():
     return parser.parse_args()
 
 
+class Scanner:
+    def __init__(self, source):
+        self.source = source
+
+    def scan_tokens(self):
+        return []
+
+
+def run(source):
+    scanner = Scanner(source)
+    tokens = scanner.scan_tokens()
+
+    for token in tokens:
+        print(token)
+
+
 def run_prompt():
-    print("run_prompt")
+    print('Welcome to the lox interpreter.  Type ctrl-d to exit')
+    while True:
+        try:
+            line = input("> ")
+        except EOFError:
+            break
+        run(line)
 
 
-def run_file(filename):
-    print("run_file: " + filename)
+def run_file(path):
+    try:
+        run(open(path).read())
+    except FileNotFoundError:
+        print(f'file not found: {path}')
+        return -1
 
 
 def main(args):
     if not args.files:
-        run_prompt()
+        return run_prompt()
     else:
         for f in args.files:
-            run_file(f)
+            ret = run_file(f)
+            if ret:
+                return ret
 
 
 if __name__ == '__main__':
