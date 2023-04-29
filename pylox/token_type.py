a3362435341232b3d12baa243c413ef65cfb9739
Bryan Murdock <bmurdock@gmail.com>
2023-04-29 08:48:59 -0600

    added token enum and class

---
+++ pylox/token_type.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class TokenType(Enum):
+    # single character tokens
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    left_brace = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+    
+    # one or two character tokens
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+
+    # literals
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+    
+    # keywords
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FLASE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+
+    EOF = auto()
+
+
+@dataclass
+class Token:
+    token_type: TokenType
+    lexeme: str
+    literal: object
+    line: int
+
+    def to_string(self):
+        return f"{self.token_type.name} {self.lexeme} {self.literal}"
+        
